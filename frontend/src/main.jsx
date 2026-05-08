aee428f3b96fd25f25ced381df474bc1e4236826
shae <shae.legowski.2024@uni.strath.ac.uk>
2026-05-08 03:53:21 +0100

    feat: add Google Analytics (GA4) across all sites

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ReactGA from 'react-ga4';
+
+
+ReactGA.initialize(import.meta.env.VITE_GA_ID);
+ReactGA.send({ hitType: 'pageview', page: '/' });
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
